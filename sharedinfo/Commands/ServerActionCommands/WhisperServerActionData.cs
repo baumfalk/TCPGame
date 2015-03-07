cd290f4a0f51047604b477826da1498c5d247e2b
genveir <maedaros@yahoo.com>
2015-03-07 14:13:05 +0100

    Not sure

--- sharedinfo/Commands/ServerActionCommands/WhisperServerActionData.cs
+++ sharedinfo/Commands/ServerActionCommands/WhisperServerActionData.cs
@@ -5,7 +5,16 @@ using System.Text;
 
 namespace TCPGameSharedInfo.Commands.ServerActionCommands
 {
-    class WhisperServerActionData
+    class WhisperServerActionData : ServerCommandData
     {
+        String target;
+        String message;
+
+        public WhisperServerActionData(String target, String message)
+        {
+            this.target = target;
+            this.message = message;
+            this.commandType = ServerCommandType.Whisper;
+        }
     }
 }
